331c46e253cc6865a267fb351d54d72a66264a25
kristine acob <kristinejfacob@outlook.com>
2018-04-09 20:56:58 -0700

    intial commit staged 9pm

---
+++ MadeWithLove/Customer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MadeWithLove
+{
+    class Customer
+    {
+        #region Properties
+       
+        public string CustomerName { get; set; }
+        public int CustomerNumber { get; set; }
+        public DateTime DateNeeded { get; set; }
+        public int CakeSize { get; set; }
+        public string CakeFlavor { get; set; }
+        public decimal CakePrice { get; set; }
+        public bool BalancePaid { get; set; }
+
+        #endregion
+    }
+}
